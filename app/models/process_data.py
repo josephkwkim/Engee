13512825070923f8fe29c1c498c35e28411037e9
Joe <jwk1@andrew.cmu.edu>
2018-01-30 21:33:53 -0500

    added models file

--- app/models/process_data.py
+++ app/models/process_data.py
@@ -1,4 +1,3 @@
-import autosklearn
 import pandas as pd
 import csv
 
@@ -16,9 +15,27 @@ def get_column_names(filename):
 
     return cols
 
-def select_columns(x_names, y_name):
+def load_dataset(filename):
+    '''
+    :param filename: name or path of file to be loaded (.csv)
+    :return: pandas dataframe representing the spreadsheet
+    '''
+    return pd.read_csv(filename)
+
+def get_first_rows(dataset):
+    '''
+    :param dataset: pandas dataframe (not separated into x and y yet)
+    :return: first 30 rows of the dataframe as a numpy ndarray
+    '''
+    return dataset.head(30).values
+
+def select_columns(x_names, y_name, dataset):
     """
     :param x_names: list of string names of feature columns
     :param y_name: string name of target column
     :return: tuple, X matrix and y vector
     """
+    y = dataset[y_name].values
+    x = dataset.drop([y_name], axis=1).values
+    return x, y
+
