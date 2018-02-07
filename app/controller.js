344bc4c1fc99e7aeecefe5b5a83483b0c322c786
Calvin Lui <calvinhklui@yahoo.com>
2018-02-06 19:20:15 -0500

    completed rough of feature selection

--- app/controller.js
+++ app/controller.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 
   // global variables
   var host = 'localhost';
+  var sel_features = [];
+  var sel_target = []; // only one item
 
   function init() {
     setupListeners();
@@ -12,6 +14,19 @@ $(document).ready(function() {
     console.log(response);
   }
 
+  // add response to sel_features
+  function selectFeature(response) {
+    sel_features.push(response);
+    console.log("Selected " + response + " as Feature!");
+  }
+
+  // assign sel_target to response
+  function selectTarget(response) {
+    sel_target.push(response);
+    console.log("Selected " + response + " as Target!");
+  }
+
+  // for generating interface after selecting dataset
   function displayData(response) {
     column_names = response.column_names;
     num_columns = column_names.length;
@@ -44,42 +59,68 @@ $(document).ready(function() {
         }
     }
 
-    // create feature buttons
+    // create feature choices
     for (var c = 0; c < num_columns; c++) {
       var button = document.createElement("button");
+      button.id = "attributeF" + c;
       button.innerHTML = all_rows[0][c];
       document.getElementById("FeaturesDiv").appendChild(button);
-      // Add event handler for feature
-      /*button.addEventListener ("click", function() {
-        alert("did something") }); */
+      listenFeature("attributeF" + c);
     }
 
+    // create choose features button
     var chooseButtonX = document.createElement("button");
+    chooseButtonX.id = "chooseButtonX";
     chooseButtonX.className = 'btn btn-primary btn-x1';
     chooseButtonX.innerHTML = "Choose Features";
     document.getElementById("FeaturesDiv").appendChild(chooseButtonX);
     setupListeners(listener="chooseButtonX");
   }
 
+  // for generating interface after selecting features
   function processFeatures(response) {
-    // create feature buttons
+    // create target choices
     for (var c = 0; c < num_columns; c++) {
-      var button = document.createElement("button");
-      button.innerHTML = all_rows[0][c];
-      document.getElementById("TargetDiv").appendChild(button);
-      // Add event handler for feature
-      /*button.addEventListener ("click", function() {
-        alert("did something") }); */
+      if (sel_features.indexOf(all_rows[0][c]) == -1) {
+        var button = document.createElement("button");
+        button.id = "attributeT" + c;
+        button.innerHTML = all_rows[0][c];
+        document.getElementById("TargetDiv").appendChild(button);
+        listenTarget("attributeT" + c);
+      }
     }
 
+    // create choose target button
     var chooseButtonY = document.createElement("button");
+    chooseButtonY.id = "chooseButtonY";
     chooseButtonY.className = 'btn btn-primary btn-x1';
     chooseButtonY.innerHTML = "Choose Target";
     document.getElementById("TargetDiv").appendChild(chooseButtonY);
+    setupListeners(listener="chooseButtonY");
   }
 
-  function setupListeners() {
+  // wrapper for feature selection setupListeners
+  function listenFeature(id) {
+    var featureChoice = document.getElementById(id);
+    if (typeof window.addEventListener==='function') {
+      featureChoice.addEventListener ("click", function() {
+        selectFeature(featureChoice.innerHTML) });
+    }
+  }
+
+  // wrapper for target selection setupListeners
+  function listenTarget(id) {
+    var targetChoice = document.getElementById(id);
+    if (typeof window.addEventListener==='function') {
+      targetChoice.addEventListener ("click", function() {
+        selectTarget(targetChoice.innerHTML) });
+    }
+  }
+
+  function setupListeners(listener="") {
+    // condition for init
     if (listener == "") {
+      // listener for iris selection
       $( "#SelectIris" ).click( function( event ) {
         console.log("FRONT: Clicked on Iris");
         $.ajax({
@@ -96,6 +137,7 @@ $(document).ready(function() {
         }).done((response) => { displayData(response) } );
       } );
 
+      // listener for file selection
       $( 'input[type=file]' ).change(function () {
         var fileName = this.files[0].name;
         console.log("FRONT: Clicked on Select and Chose " + fileName);
@@ -111,40 +153,43 @@ $(document).ready(function() {
           dataType: 'json',
         }).done((response) => { displayData(response) } );
       } );
-
-      $( "#ConfirmButton" ).click( function( event ) {
-        var features = document.getElementById("FeaturesText");
-        var target = document.getElementById("TargetText");
-        console.log("FRONT: Confirmed Features and Target ");
-        $.ajax({
-          url: 'http://' + host + ':5000/',
-          type: 'POST',
-          data: JSON.stringify({
-            name: "Confirmed Features and Target",
-            phase: 2,
-            features: features.value,
-            target: target.value
-            }),
-          contentType: 'application/json',
-          dataType: 'json',
-        }).done((response) => { process(response) } );
-      } );
     }
 
-    // set up chooseButtonX's listener
+    // condition for selecting features
     if (listener == "chooseButtonX") {
       $( "#chooseButtonX" ).click( function( event ) {
-        console.log("CHOSE FEATURES!");
+        console.log("Chose Features!");
+        //var features = document.getElementById("FeaturesText");
+        //var target = document.getElementById("TargetText");
         $.ajax({
           url: 'http://' + host + ':5000/',
           type: 'POST',
           data: JSON.stringify({
             name: "Chose Features",
             phase: 2,
+            features: sel_features
             }),
           contentType: 'application/json',
           dataType: 'json',
         }).done((response) => { processFeatures(response) } );
+      } );
+    }
+
+    // condition for selecting features
+    if (listener == "chooseButtonY") {
+      $( "#chooseButtonY" ).click( function( event ) {
+        console.log("Chose Target!");
+        $.ajax({
+          url: 'http://' + host + ':5000/',
+          type: 'POST',
+          data: JSON.stringify({
+            name: "Chose Target",
+            phase: 2,
+            target: sel_target
+            }),
+          contentType: 'application/json',
+          dataType: 'json',
+        }).done((response) => { processTarget(response) } );
       } );
     }
 
